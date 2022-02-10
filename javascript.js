dcfc3655a2fda0091cf0dc2546e1f5dcdc98b13d
Leonardo Gatica <lgatica@protonmail.com>
2022-02-10 11:30:50 -0300

    fix(deps): update eslint to v8 and configs/plugins
    Update eslint-config-standard to 17.0.0-1
    Update eslint-plugin-promise to 6.0.0
    Include config from eslint-config-jsdoc-strict
    BREAKING CHANGE: Update eslint to v8

--- javascript.js
+++ javascript.js
@@ -1,11 +1,65 @@
 module.exports = {
-  extends: ['./common', 'jsdoc-strict'],
+  extends: ['./common'],
   settings: {
     jsdoc: {
       preferredTypes: {
-        object: 'Object'
+        object: 'Object',
       },
-      mode: 'typescript'
-    }
-  }
+      mode: 'typescript',
+    },
+  },
+  plugins: ['jsdoc'],
+  rules: {
+    'require-jsdoc': [
+      'error',
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: true,
+          FunctionExpression: true,
+        },
+      },
+    ],
+    'valid-jsdoc': [
+      'error',
+      {
+        prefer: {
+          arg: 'param',
+          argument: 'param',
+          constructor: 'class',
+          return: 'returns',
+          virtual: 'abstract',
+        },
+        preferType: {
+          Boolean: 'boolean',
+          Number: 'number',
+          String: 'string',
+          function: 'Function',
+          object: 'Object',
+          array: 'Array',
+          date: 'Date',
+          regexp: 'RegExp',
+          symbol: 'Symbol',
+        },
+      },
+    ],
+    'jsdoc/check-param-names': 'error',
+    'jsdoc/check-tag-names': 'error',
+    'jsdoc/check-types': 'error',
+    'jsdoc/newline-after-description': 'error',
+    'jsdoc/no-undefined-types': 'error',
+    'jsdoc/require-description-complete-sentence': 'error',
+    'jsdoc/require-example': 'error',
+    'jsdoc/require-hyphen-before-param-description': 'error',
+    'jsdoc/require-param-description': 'error',
+    'jsdoc/require-param-name': 'error',
+    'jsdoc/require-param-type': 'error',
+    'jsdoc/require-param': 'error',
+    'jsdoc/require-returns-description': 'error',
+    'jsdoc/require-returns-type': 'error',
+    'jsdoc/require-returns': 'error',
+    'jsdoc/valid-types': 'error',
+  },
 }
